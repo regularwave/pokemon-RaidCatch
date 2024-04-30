a996c72b381523711e103ce9f665a516d6d0ffc7
regularwave <regularwave@gmail.com>
2024-04-29 23:21:47 -0400

    it works!

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
 var resultsDisplay = document.getElementById('results');
+var pokeAMDropdown = document.getElementById('ampokemon');
+var pokeNZDropdown = document.getElementById('nzpokemon');
 var raids;
 
-function test() {
-    resultsDisplay.innerHTML = "script ran";
+async function setup() {
+    raids = await parseraids();
+
+    raids.forEach(poke => {
+        let option = document.createElement('option');
+        option.value = poke.Name;
+        if ("abcdefghijklm".includes(poke.Name[0].toLowerCase())) {
+            pokeAMDropdown.appendChild(option);
+        } else {
+            pokeNZDropdown.appendChild(option);
+        }
+    });
+
+    document.getElementById('amname').addEventListener('input', function () {
+        console.log('changed to ' + document.getElementById('amname').value);
+        document.getElementById('nzname').value = '';
+        renderResults(document.getElementById('amname').value);
+    });
+    document.getElementById('nzname').addEventListener('input', function () {
+        console.log('changed to ' + document.getElementById('nzname').value);
+        document.getElementById('amname').value = '';
+        renderResults(document.getElementById('nzname').value);
+    });
 }
 
-async function dothething() {
-    const response = await fetch("./dens.json");
+async function parseraids() {
+    const response = await fetch("./data.json");
     const content = await response.json();
     return content;
 }
 
-async function button() {
-    raids = await dothething();
-    console.log(raids);
+function renderResults(pname) {
+    resultsDisplay.innerHTML = "";
+    filtered = raids.filter((e) => e.Name.toLowerCase() == pname.toLowerCase());
+    try {
+        resultsDisplay.innerHTML += filtered[0].Name + ":<br>";
+        (filtered[0].Sources).forEach(source => {
+            resultsDisplay.innerHTML += source[0] + " " + source[1] + " " + source[2] + "<br>";
+        })
+    } catch (error) {
+        resultsDisplay.innerHTML = "not a valid pokemon or pokemon is not in a raid";
+    }
+}
+
+function button() {
+    resultsDisplay.innerHTML = "";
+    raids.forEach(poke => {
+        resultsDisplay.innerHTML += poke.Name + "<br>";
+    })
 }
